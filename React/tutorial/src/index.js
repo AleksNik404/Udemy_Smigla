3c4b0585660b157e8228e6223d7f1a9ace7e8305
AleksNik404 <AleksNik404@gmail.com>
2022-11-07 13:57:39 +0300

    REACT: feat: react syntax (props, map loop) - [ tutorial-book ]

--- React/tutorial/src/index.js
+++ React/tutorial/src/index.js
@@ -1,45 +1,47 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
+
+const books = [
+  {
+    id: 1,
+    title: 'Turkey Trouble',
+    author: 'by Matthew Perry',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91QEOvkzI9L._AC_UL127_SR127,127_.jpg',
+  },
+  {
+    id: 2,
+    title: 'Friends, Lovers, and the Big Terrible Thing',
+    author: 'by Wendi Silvano',
+    img: 'https://m.media-amazon.com/images/P/B09M1RPWWL.01._SCLZZZZZZZ_SX500_.jpg',
+  },
+  {
+    id: 3,
+    title: 'Brown Bear, Brown Bear, What Do You See?',
+    author: 'by Bill Martin Jr.',
+    img: 'https://m.media-amazon.com/images/P/0805047905.01._SCLZZZZZZZ_SX500_.jpg',
+  },
+];
 
 export function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = () => (
-  <article className="book">
-    <Image />
-    <Title />
-    <Author />
-  </article>
-);
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/91QEOvkzI9L._AC_UL127_SR127,127_.jpg"
-    alt="Turkey book"
-  />
-);
-const Title = () => <h3>Turkey Trouble</h3>;
-const Author = () => <p>by Wendi Silvano</p>;
+const Book = (props) => {
+  let { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt="Turkey book" />
+      <h3>{title}</h3>
+      <p>{author}</p>
+    </article>
+  );
+};
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(<BookList />);
