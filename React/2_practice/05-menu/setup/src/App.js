dbd7e87ee4d430248c142676926df9c1f8975e5c
AleksNik404 <AleksNik404@gmail.com>
2022-11-21 12:03:23 +0300

    REACT: feat: flexible menu - [ micro practice ]

--- React/2_practice/05-menu/setup/src/App.js
+++ React/2_practice/05-menu/setup/src/App.js
@@ -3,9 +3,21 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItmes, setMenuItmes] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItmes(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+
+    setMenuItmes(newItems);
+  };
 
   return (
     <menu>
@@ -14,7 +26,7 @@ function App() {
           <h2 className="">our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItmes} />
       </section>
     </menu>
