edc764d6fe57fd43973e0288793d0b8899a06948
AleksNik404 <AleksNik404@gmail.com>
2022-12-02 14:07:36 +0300

    REACT: feat: check slider and control form - [ micro practice ]

--- React/1_tutorials/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ React/1_tutorials/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,49 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !email) console.log('empty values');
+    else {
+      setPeople((people) => [...people, { id: new Date().getTime(), name, email }]);
+      console.log(people);
+
+      setEmail('');
+      setName('');
+    }
+  };
+
+  const changeName = (e) => setName(e.target.value);
+  const changeEmail = (e) => setEmail(e.target.value);
+
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name: </label>
+            <input type="text" id="firstName" name="firstName" value={name} onChange={changeName} />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email: </label>
+            <input type="text" id="email" name="email" value={email} onChange={changeEmail} />
+          </div>
+          <button type="submit">add person</button>
+        </form>
+        {people.map(({ id, name, email }) => (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <p>{email}</p>
+          </div>
+        ))}
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
