5534436ef006ef24f336376d0634430ef6d4b549
AleksNik404 <AleksNik404@gmail.com>
2022-12-06 15:09:47 +0300

    REACT: feat: useReducer - [ tutorial ]

--- React/1_tutorials/src/tutorial/6-useReducer/setup/Modal.js
+++ React/1_tutorials/src/tutorial/6-useReducer/setup/Modal.js
@@ -1,7 +1,19 @@
 import React, { useEffect } from 'react';
 
-const Modal = () => {
-  return <div>i'm modal</div>;
+const Modal = ({ modalContent, closeModal }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      closeModal();
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <div className="modal">
+      <p>{modalContent}</p>
+    </div>
+  );
 };
 
 export default Modal;
