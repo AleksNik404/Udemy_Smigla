4ddc3b7393c3dc76e1684a34f4fc16eb3c5baa1e
AleksNik404 <AleksNik404@gmail.com>
2022-12-11 14:22:42 +0300

    REACT: feat: Stripe nav menu - useContext - [ micro practice ]

--- React/2_practice/13-stripe-submenus/setup/src/context.js
+++ React/2_practice/13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,48 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidemenuOpen, setIsSidemenuOpen] = useState(false);
+
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: '', links: [] });
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSidemenu = (text, coord) => {
+    const page = sublinks.find((link) => link.page === text);
+    setLocation(coord);
+    setPage(page);
+    setIsSidemenuOpen(true);
+  };
+  const closeSidemenu = () => {
+    setIsSidemenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidemenuOpen,
+        isSidebarOpen,
+        openSidemenu,
+        openSidebar,
+        closeSidemenu,
+        closeSidebar,
+        location,
+        page,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
