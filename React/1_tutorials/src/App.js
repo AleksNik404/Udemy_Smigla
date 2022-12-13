be951fc7ece53ccdad232319d693ae04d5e69d49
AleksNik404 <AleksNik404@gmail.com>
2022-12-13 15:33:12 +0300

    REACT: init: store proj

--- React/1_tutorials/src/App.js
+++ React/1_tutorials/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import Setup from './tutorial/8-useContext/setup/1-context-api';
-
+import Setup from './tutorial/12-memo-useMemo-useCallback/setup/index';
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
       <Setup />
     </div>
   );
