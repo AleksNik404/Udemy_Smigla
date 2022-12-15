d86911eff9f172ca45761a171e21d015b6788137
AleksNik404 <AleksNik404@gmail.com>
2022-12-15 14:40:50 +0300

    REACT: feat: home page and one product page

--- React/3_react-course-comfy-sloth-starter-main/src/components/Contact.js
+++ React/3_react-course-comfy-sloth-starter-main/src/components/Contact.js
@@ -1,9 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join put newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, dolorem
+            perferendis sed nemo, soluta a in asperiores laborum, unde minus at voluptate eos
+            expedita nam veniam magnam earum eveniet? Harum!
+          </p>
+          <form className='contact-form' action='https://formspree.io/f/xrgdnnrp' method='POST'>
+            <input type='email' className='form-input' placegolder='enter email' name='email' />
+            <button type='submit' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +86,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
--- React/3_react-course-comfy-sloth-starter-main/src/components/Services.js
+++ React/3_react-course-comfy-sloth-starter-main/src/components/Services.js
@@ -1,10 +1,35 @@
-import React from 'react'
-import styled from 'styled-components'
-import { services } from '../utils/constants'
+import React from 'react';
+import styled from 'styled-components';
+import { services } from '../utils/constants';
 
 const Services = () => {
-  return <h4>services </h4>
-}
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <article className='header'>
+          <h3>custom furniture built only for you</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, molestiae mollitia
+            obcaecati repellat officiis odio voluptatem saepe pariatur distinctio porro modi
+            quibusdam excepturi totam ad, vel quasi ipsa! A, molestias.
+          </p>
+        </article>
+        <div className='services-center'>
+          {services.map((service) => {
+            const { id, icon, title, text } = service;
+            return (
+              <article key={id} className='service'>
+                <span className='icon'>{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   h3,
@@ -68,5 +93,5 @@ const Wrapper = styled.section`
       transform: translateY(5rem);
     }
   }
-`
-export default Services
+`;
+export default Services;
