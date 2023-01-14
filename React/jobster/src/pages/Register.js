3ebf628534b872854cbc03c65dbc8ce7ef612955
AleksNik404 <AleksNik404@gmail.com>
2023-01-14 11:33:37 +0300

    React: feat: add route layout

--- React/jobster/src/pages/Register.js
+++ React/jobster/src/pages/Register.js
@@ -6,6 +6,7 @@ import Wrapper from '../assets/wrappers/RegisterPage';
 import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser, registerUser } from '../features/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 const initialState = {
   name: '',
@@ -18,6 +19,7 @@ const Register = () => {
   const [values, setValues] = useState(initialState);
   const { user, isLoading } = useSelector((store) => store.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -44,6 +46,13 @@ const Register = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
 
+  useEffect(() => {
+    if (!user) return;
+
+    const timer = setTimeout(() => navigate('/'), 2000);
+    return () => clearTimeout(timer);
+  }, [user]);
+
   return (
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>
@@ -58,8 +67,8 @@ const Register = () => {
         {/* prettier-ignore */}
         <FormRow type='password' name='password' value={values.password} handleChange={handleChange} />
 
-        <button type='submit' className='btn btn-block'>
-          submit
+        <button type='submit' className='btn btn-block' disabled={isLoading}>
+          {isLoading ? 'loading...' : 'submit'}
         </button>
         <p>
           {values.isMember ? 'Not a member yet?' : 'Already a member?'}
