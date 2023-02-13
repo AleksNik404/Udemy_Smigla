24b46ff2960dd0193273486e723d2ddf7243e14b
AleksNik404 <AleksNik404@gmail.com>
2023-02-13 17:32:04 +0300

    React: feat: add stats page with recharts

--- React/jobster/src/utils/axios.js
+++ React/jobster/src/utils/axios.js
@@ -1,5 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
+import { getUserFromLocalStorage } from "./localStorage";
 
 export const customFetch = axios.create({
-  baseURL: 'https://jobify-prod.herokuapp.com/api/v1/toolkit',
+  baseURL: "https://jobify-prod.herokuapp.com/api/v1/toolkit",
 });
+
+customFetch.interceptors.request.use(
+  (config) => {
+    const user = getUserFromLocalStorage();
+    if (user) {
+      config.headers["Authorization"] = `Bearer ${user.token}`;
+      // in the latest version "common" returns undefined
+      // config.headers.common['Authorization'] = `Bearer ${user.token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
